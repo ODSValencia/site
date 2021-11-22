0c973dd246dd4899e66df04c4cdc785add5d55c8
Viciano <94388326+Viciano@users.noreply.github.com>
2021-11-22 16:01:27 +0000

    Deploying to gh-pages from  @ bdfc1f222ce4f8359e3728dcdbaaf17487ca0153 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2850,7 +2850,7 @@ var mapView = function () {
     $('#map').sdgMap({
       indicatorId: indicatorId,
       mapOptions: {"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
-      mapLayers: [],
+      mapLayers: [{"geojson_file":"https://github.com/ODSValencia/data/Distritos.geojson","name_property":"nombre","id_property":"coddistrit","output_subfolder":"Distritos","filename_prefix":"indicator_"}],
       precision: precision,
       decimalSeparator: decimalSeparator,
     });
